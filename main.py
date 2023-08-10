71c243249861f77a8c9d6b1abc0581c515e0cf88
trofimleg0 <trofimlego@gmail.com>
2023-08-10 21:41:04 +0200

    Added check_for_redirect_func and requirements

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import os
 import requests
 from pathlib import Path
 from dotenv import load_dotenv
+from urllib.error import URLError
+
+def check_for_redirect(response):
+    if len(response.history) > 0:
+        return True
+    return False
 
 if __name__ == '__main__':
     load_dotenv()
@@ -9,9 +15,15 @@ if __name__ == '__main__':
     image_path = os.environ['IMAGE_PATH']
     Path(image_path).mkdir(parents=True, exist_ok=True)
     
-    for i in range(1, 11):
-        with open(f'{image_path}/id{i}.txt', 'w') as file:
-            url = f'https://tululu.org/txt.php?id={i}'
+    
+    for book_id in range(1, 11):
+        url = f'https://tululu.org/txt.php?id={book_id}'
+        try:
             response = requests.get(url)
             response.raise_for_status()
-            file.write(response.text)
+            if not check_for_redirect(response):
+                with open(f'{image_path}/id{book_id}.txt', 'w') as file:
+                    file.write(response.text)
+        except Exception as ex:
+            raise requests.exceptions.HTTPError(ex)
+        
