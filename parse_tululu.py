f7936cb78660e238fea1c35a5daa9f3695772c22
trofimleg0 <trofimlego@gmail.com>
2023-08-19 17:33:46 +0200

    Download books from all pages

---
+++ parse_tululu.py
@@ -0,0 +1,123 @@
+import os
+import json
+import requests
+
+from bs4 import BeautifulSoup
+from argparse import ArgumentParser
+from pathlib import Path
+from dotenv import load_dotenv
+from urllib.parse import urljoin
+from pathvalidate import sanitize_filepath
+
+
+def get_args():
+    parser = ArgumentParser("Select a range of IDs to download books")
+    parser.add_argument(
+        "-s", "--start_id", default="1", type=int, help="IDs start range"
+    )
+    parser.add_argument(
+        "-e", "--end_id", default="10", type=int, help="IDs end range"
+    )
+    args = parser.parse_args()
+
+    return args.start_id, args.end_id
+
+
+def check_for_redirect(response):
+    if len(response.history) > 0:
+        return True
+    return False
+
+
+def get_book_params(soup, url, books_folder, images_folder):
+    relative_img_url = soup.find(class_="bookimage").find("img")["src"]
+    img_url = urljoin(url, relative_img_url)
+    img_name = relative_img_url.split("/")[-1]
+    
+    title_and_author = (
+        soup.find("td", class_="ow_px_td")
+        .find("div", id="content")
+        .find("h1")
+        .text.split("::")
+    )
+    title, author = map(str.strip, title_and_author)
+
+    book_genres_soup = soup.find("span", class_="d_book").find_all("a")
+    genres = []
+    for book_genre_soup in book_genres_soup:
+        genres.append(book_genre_soup.text)
+
+    book_comments_soup = soup.find_all("div", class_='texts')
+    comments = []
+    for book_comment_soup in book_comments_soup:
+        comments.append(book_comment_soup.find("span", class_="black").text)
+
+    img_src = os.path.join(images_folder, img_name)
+    book_path = os.path.join(books_folder, sanitize_filepath(title))
+
+    return title, author, genres, comments, img_name, img_src, book_path, img_url
+
+
+def download_image(url, img_name, path, images_folder_name):
+    response = requests.get(url)
+    response.raise_for_status()
+
+    img_path = os.path.join(path, images_folder_name)
+    Path(img_path).mkdir(parents=True, exist_ok=True)
+    if not check_for_redirect(response):
+        with open(f"{img_path}/{img_name}", "wb") as file:
+            file.write(response.content)
+
+
+def download_txt(book_id, filename, path, books_folder_name):
+    url = "https://tululu.org/txt.php"
+    params = {"id": book_id}
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+
+    book_path = os.path.join(path, books_folder_name)
+    Path(book_path).mkdir(parents=True, exist_ok=True)
+    if not check_for_redirect(response):
+        book_name = sanitize_filepath(filename)
+        with open(f"{book_path}/{book_name}.txt", "w") as file:
+            file.write(response.text)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    path = os.environ["WD"]
+    books_folder_name = os.environ["BOOKS"]
+    images_folder_name = os.environ["IMAGES"]
+
+    start_id, end_id = get_args()
+    all_books_params = []
+    for book_id in range(start_id, end_id + 1):
+        url = f"https://tululu.org/b{book_id}/"
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            
+            if not check_for_redirect(response):
+                soup = BeautifulSoup(response.text, "lxml")
+                title, author, genres, comments, img_name, img_src, book_path, img_url = get_book_params(
+                    soup, 
+                    url,
+                    books_folder_name, 
+                    images_folder_name
+                )
+                book_params = {
+                    "title": title,
+                    "author": author,
+                    "genres": genres,
+                    "comments": comments,
+                    "img_src": img_src,
+                    "book_path": book_path,
+                }
+                all_books_params.append(book_params)
+                # download_image(img_url, img_name, path, images_folder_name)
+                # download_txt(book_id, f"{book_id}.{title}", path, books_folder_name)
+        except Exception as ex:
+            raise requests.exceptions.HTTPError(ex)
+    all_books_params_json = "books_info.json"
+    with open(f"{path}/{all_books_params_json}", "w") as file:
+        file.write(json.dumps(all_books_params, ensure_ascii=False))
--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,28 +1,78 @@
 import os
+import json
 import requests
 
 from bs4 import BeautifulSoup
-from pathlib import Path
+from argparse import ArgumentParser
 from dotenv import load_dotenv
 from urllib.parse import urljoin
-from argparse import ArgumentParser
-from pathvalidate import sanitize_filepath
+from parse_tululu import check_for_redirect, download_image, download_txt,  get_book_params
+
+def get_args():
+    parser = ArgumentParser("Select a range of pages to download books")
+    parser.add_argument(
+        "-s", "--start_page", default="1", type=int, help="Pages start range"
+    )
+    parser.add_argument(
+        "-e", "--end_page", default="4", type=int, help="Pages end range"
+    )
+    args = parser.parse_args()
+
+    return args.start_page, args.end_page
+
+
+def extract_number_from_string(input_string):
+    number = ''.join(filter(str.isdigit, input_string))
+    return int(number) if number else None
+
 
 if __name__ == "__main__":
-    for page in range(1, 4):
+    load_dotenv()
+    path = os.environ["WD"]
+    books_folder_name = os.environ["BOOKS"]
+    images_folder_name = os.environ["IMAGES"]
+
+    start_page, end_page = get_args()
+    all_books_params = []
+    for page in range(start_page, end_page + 1):
         url = f"https://tululu.org/l55/{page}/"
         try:
             response = requests.get(url)
             response.raise_for_status()
 
             soup = BeautifulSoup(response.text, "lxml")
-            relative_book_urls = soup.find("div", id="content").find_all(
+            soup_book_urls = soup.find("div", id="content").find_all(
                 "table", class_="d_book"
             )
-            for relative_book_url in relative_book_urls:
-                book_url = urljoin(url, relative_book_url.find("a")["href"])
+        
+            for soup_book_url in soup_book_urls:
+                relative_book_url = soup_book_url.find("a")["href"]
+                book_url = urljoin(url, relative_book_url)
                 response = requests.get(book_url)
                 response.raise_for_status()
-                print(book_url)
+
+                if not check_for_redirect(response):
+                    soup = BeautifulSoup(response.text, "lxml")
+                    title, author, genres, comments, img_name, img_src, book_path, img_url = get_book_params(
+                        soup, 
+                        url,
+                        books_folder_name, 
+                        images_folder_name
+                    )
+                    book_params = {
+                    "title": title,
+                    "author": author,
+                    "genres": genres,
+                    "comments": comments,
+                    "img_src": img_src,
+                    "book_path": book_path,
+                    }
+                    all_books_params.append(book_params)
+                    book_id = extract_number_from_string(relative_book_url)
+                    # download_image(img_url, img_name, path, images_folder_name)
+                    # download_txt(book_id, f"{book_id}.{title}", path, books_folder_name)
         except Exception as ex:
             raise requests.exceptions.HTTPError(ex)
+    all_books_params_json = "books_info_by_category.json"
+    with open(f"{path}/{all_books_params_json}", "w") as file:
+        file.write(json.dumps(all_books_params, ensure_ascii=False))
