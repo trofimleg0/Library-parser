6e7701762947ad9b3317dadef7380e01d6787e90
trofimleg0 <trofimlego@gmail.com>
2023-09-03 19:55:52 +0200

    Split books by two columns

--- render_website.py
+++ render_website.py
@@ -1,7 +1,7 @@
 import json
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
-
+from more_itertools import chunked
 
 def on_reload():
     with open("books_info_by_category.json", "r") as file:
@@ -9,8 +9,10 @@ def on_reload():
     env = Environment(
         loader=FileSystemLoader("."), autoescape=select_autoescape(["html"])
     )
+
+    grouped_books = list(chunked(books, 2))
     template = env.get_template("template.html")
-    rendered_page = template.render(books=books)
+    rendered_page = template.render(grouped_books=grouped_books)
 
     with open("index.html", "w", encoding="utf8") as file:
         file.write(rendered_page)
@@ -21,4 +23,4 @@ if __name__ == "__main__":
 
     server = Server()
     server.watch("./template.html", on_reload)
-    server.serve(root="./index.html")
+    server.serve(root=".")
