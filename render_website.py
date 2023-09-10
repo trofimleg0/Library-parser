805377bed5cb545909b1e3b31bb5e695185713a4
trofimleg0 <trofimlego@gmail.com>
2023-09-10 22:35:41 +0200

    Split books list by pages.

--- render_website.py
+++ render_website.py
@@ -1,7 +1,9 @@
+import os
 import json
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 from more_itertools import chunked
+
 
 def on_reload():
     with open("books_info_by_category.json", "r") as file:
@@ -10,12 +12,17 @@ def on_reload():
         loader=FileSystemLoader("."), autoescape=select_autoescape(["html"])
     )
 
-    grouped_books = list(chunked(books, 2))
-    template = env.get_template("template.html")
-    rendered_page = template.render(grouped_books=grouped_books)
+    # Create a directory for books
+    os.makedirs('pages', exist_ok=True)
 
-    with open("index.html", "w", encoding="utf8") as file:
-        file.write(rendered_page)
+    books_pages = list(chunked(books, 20))
+    for num, books_on_page in enumerate(books_pages, 1):
+        grouped_books = list(chunked(books_on_page, 2))
+        template = env.get_template("template.html")
+        rendered_page = template.render(grouped_books=grouped_books)
+
+        with open(f"./pages/index{num}.html", "w", encoding="utf8") as file:
+            file.write(rendered_page)
 
 
 if __name__ == "__main__":
